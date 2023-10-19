4fc654ee621f7c871d4ab7f479e77d4b66646334
Anton LeBeque <144728558+AntonLeB04@users.noreply.github.com>
2023-10-19 12:57:19 -0500

    Got the csv file to read

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,37 @@ AvgChange = []
 GreatestIncrease = []
 GreatestDecrease = []
 
-budget_data = os.path.join("..", "Resources", "budget_data.csv")
-with open(budget_data) as csvfile:
+budget = os.path.join("Resources", "budget_data.csv")
+
+
+
+
+with open(budget) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    for row in csvreader:
+        # Add place
+        Total.append(row[0])
+
+        # Add population
+        TotalMonths.append(row[1])
+
+        # Average Change
+        AvgChange = round(int(row[0]) / int(row[1]))
+
+        print(AvgChange)
+        
+cleanedpath = list(zip(Total,TotalMonths,AvgChange))
+
+# Set variable for output file
+output_file = os.path.join('Analysis')
+
+#  Open the output file
+with open(output_file, "w", newline='') as Analysis:
+    writer = csv.writer(Analysis)
+
+    # Write the header row
+    writer.writerow([Analysis])
+
+    # Write in zipped rows
+    writer.writerows(cleanedpath)
