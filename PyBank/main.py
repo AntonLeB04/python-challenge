2a94f2f87f5178276ccdbb20cd8bd5d7f39ebe79
Anton LeBeque <144728558+AntonLeB04@users.noreply.github.com>
2023-10-22 14:57:43 -0500

    Got PyBank done!

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,8 @@ import csv
 TotalMonths = []
 Total = []
 AvgChange = []
-GreatestIncrease = []
-GreatestDecrease = []
+GreatestIncrease = ["",0]
+GreatestDecrease = ["",0]
 
 # Reading the csv file
 budget_csv = os.path.join("Resources", "budget_data.csv")
@@ -18,29 +18,58 @@ with open(budget_csv) as csvfile:
 
     headline = csvfile.__next__()
     Total = 0
+    TotalMonths = 0
+    PreviousProfit = 0
+    change = 0
+    totalchange = 0
+    counter = 0
 
     for row in csvreader:
         # Sum of Total taken from Stack Overflow 'Sum a csv column in python' answered by Martijn Pieters
         Total += int((row[1]))
 
-        # Add population
-        TotalMonths = len(row[0])
+        # Total Months
+        TotalMonths += 1 
+
+        currentprofit = int((row[1]))
+
+        if PreviousProfit != 0:
+            change = currentprofit - PreviousProfit
+            totalchange += change
+            counter += 1
+
+        PreviousProfit = currentprofit
+
+        if change > GreatestIncrease[1]:
+            GreatestIncrease[1] = change
+            GreatestIncrease[0] = row[0]
+
+        if change < GreatestDecrease[1]:
+            GreatestDecrease[1] = change
+            GreatestDecrease[0] = row[0]
 
         # Average Change
         #AvgChange = round(int(row[1]) / int(row[0]))
 
-        print(TotalMonths)
+print(GreatestDecrease)
 
 cleaned_csv = list(zip(str((Total, TotalMonths))))
+
+output = f"""
+Financial Analysis
+----------------------------
+Total Months: {TotalMonths}
+Total: ${Total}
+Average Change: ${totalchange/counter:.2f}
+Greatest Increase in Profits: {GreatestIncrease[0]} (${GreatestIncrease[1]})
+Greatest Decrease in Profits: {GreatestDecrease[0]} (${GreatestDecrease[1]})
+"""
+
+print(output)
 
 output_file = os.path.join("Analysis", "Financial Analysis")
 
 #  Open the output file
 with open(output_file, "w", newline='') as datafile:
-    writer = csv.writer(datafile)
-
-    # Write the header row
-    writer.writerow(["Analysis for Financial Records"])
-
-    # Write in zipped rows
-    writer.writerows(cleaned_csv)
+    
+    datafile.write(output)
